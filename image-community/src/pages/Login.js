f341b255b5d0e4728e564c8da9fee12481fd42d4
kimhanjune93 <kimhanjune93@gmail.com>
2021-10-01 16:09:14 +0900

    add, update: cookie에 id,pw 저장, 파싱 및 조회, 삭제

--- image-community/src/pages/Login.js
+++ image-community/src/pages/Login.js
@@ -1,6 +1,23 @@
 import React from "react";
 import { Input, Text, Grid, Button } from "../elements";
+import {getCookie, setCookie, deleteCookie} from "../shared/Cookie";
 const Login = (props) => {
+  const [id,setId] = React.useState('');
+  const [pwd,setPwd] = React.useState('');
+  console.log(getCookie('USER_ID'));
+  const changeId= (e) => {
+    setId(e.target.value);
+  }
+
+  const changePwd= (e) => {
+    setPwd(e.target.value);
+  }
+
+  const login =() => {
+    setCookie("USER_ID",id,3);
+    setCookie("USER_PWD",pwd,3);
+  }
+
   return (
     <React.Fragment>
       <Grid padding="16px">
@@ -9,23 +26,21 @@ const Login = (props) => {
         </Text>
         <Grid padding="16px 0px">
           <Input
+            value={pwd}
             label="아이디"
             placeholder="아이디를 입력해주세요."
-            _onChange={() => {
-              console.log("아이디");
-            }}
+            _onChange={changeId}
           />
         </Grid>
         <Grid padding="16px 0px">
           <Input
+            value={pwd}
             label="패스워드"
             placeholder="암호를 입력해주세요."
-            _onChange={() => {
-              console.log("비번");
-            }}
+            _onChange={changePwd}
           />
         </Grid>
-        <Button text="로그인하기" _onClick = {()=>{console.log("로그인 했어")}}/>
+        <Button text="로그인하기" _onClick = {login}/>
       </Grid>
     </React.Fragment>
   );
