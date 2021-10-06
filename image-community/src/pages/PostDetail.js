7b9428502b9f106a170f1321101a380a5f821fed
kimhanjune93 <kimhanjune93@gmail.com>
2021-10-06 21:55:19 +0900

    update : postDetail 수정, firestore 단일 데이터 가져오기

--- image-community/src/pages/PostDetail.js
+++ image-community/src/pages/PostDetail.js
@@ -2,12 +2,46 @@ import React from "react";
 import Post from "../components/Post";
 import CommentWrite from "../components/CommentWrite";
 import CommentList from "../components/CommentList";
-import { Grid, Image, Text } from "../elements";
+import {doc, query, where, getDoc} from "firebase/firestore";
+import { useSelector } from "react-redux";
+import {db} from "../shared/firebase";
 const PostDetail = (props) => {
-    console.log(props);
+  const id=props.match.params.id;
+  const post_list=useSelector(store => store.post.list);
+  const user_info = useSelector(state => state.user.user);
+  const post_idx = post_list.findIndex(p=>p.id === id);
+  const post_data = post_list[post_idx];
+  const [post, setPost] = React.useState(post_data? post_data : null);
+
+  React.useEffect(()=> {
+    if(post) {
+      return;
+    }
+    return async function() {
+    const postDB = doc(db, "post",id);
+    const postDoc = await getDoc(postDB);
+    let _post = postDoc.data();
+    let post = Object.keys(_post).reduce(
+      // reduce 누산기
+      (acc, cur) => {
+        if (cur.indexOf("user_") !== -1) {
+          return {
+            ...acc,
+            user_info: { ...acc.user_info, [cur]: _post[cur] },
+          };
+        }
+        return { ...acc, [cur]: _post[cur] };
+      },
+      { id: doc.id, user_info: {} }
+    );
+    setPost(post);
+    }
+    // query(postDB, where("","",""))
+  });
+
   return (
     <React.Fragment>
-      <Post/>
+      {post && <Post {...post} is_me={post.user_info.user_id === user_info.uid}/>}
       <CommentWrite/>
       <CommentList/>
     </React.Fragment>
--- image-community/src/pages/PostList.js
+++ image-community/src/pages/PostList.js
@@ -4,13 +4,15 @@ import { useDispatch, useSelector } from "react-redux";
 import Post from "../components/Post";
 import { actionCreators as postActions } from "../redux/modules/post";
 import InfinityScroll from "../shared/InfinityScroll";
-
+import { Grid } from "../elements";
 const PostList = (props) => {
   const dispatch = useDispatch();
   const post_list = useSelector((state) => state.post.list);
   const user_info = useSelector((state) => state.user.user);
   const is_loading = useSelector((state) => state.post.is_loading);
   const paging = useSelector((state) => state.post.paging);
+
+  const { history } = props;
   console.log(is_loading);
   React.useEffect(() => {
     if (post_list.length === 0) {
@@ -19,22 +21,45 @@ const PostList = (props) => {
   }, []);
   return (
     <React.Fragment>
-      {/* <Post/> */}
-      <InfinityScroll
-        callNext = {() => {
+      <Grid bg="#EFF6FF" padding ="20px 0px">
+        {/* <Post/> */}
+        <InfinityScroll
+          callNext={() => {
             dispatch(postActions.getPostFB(paging.next));
-        }}
-        is_next={paging.next?true:false}
-        loading={is_loading}
-      >
-        {post_list.map((p, idx) => {
-          if (p.user_info.user_id === user_info?.uid) {
-            return <Post key={p.id} {...p} is_me />;
-          } else {
-            return <Post key={p.id} {...p} />;
-          }
-        })}
-      </InfinityScroll>
+          }}
+          is_next={paging.next ? true : false}
+          loading={is_loading}
+        >
+          {post_list.map((p, idx) => {
+            if (p.user_info.user_id === user_info?.uid) {
+              return (
+                <Grid
+                bg="#ffffff"
+                  key={p.id}
+                  _onClick={() => {
+                    history.push(`/post/${p.id}`);
+                  }}
+                  margin ="0px 0px 5px 0px"
+                >
+                  <Post {...p} is_me />
+                </Grid>
+              );
+            } else {
+              return (
+                <Grid
+                bg="#ffffff"
+                  key={p.id}
+                  _onClick={() => {
+                    history.push(`/post/${p.id}`);
+                  }}
+                >
+                  <Post {...p} />
+                </Grid>
+              );
+            }
+          })}
+        </InfinityScroll>
+      </Grid>
     </React.Fragment>
   );
 };
